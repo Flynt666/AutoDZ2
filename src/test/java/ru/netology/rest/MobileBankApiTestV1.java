1a462b0e7aa76d9427aae40bb48f1f92a7e902fd
Flynt666 <bang-kirov@bk.ru>
2022-09-04 18:20:34 +0300

    Valid RUB&USD

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
 
 class MobileBankApiTestV1 {
     @Test
@@ -17,9 +19,37 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
               .then()
-              .statusCode(404)
+              .statusCode(200)
               // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
               .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
+    }
+
+    @Test
+    void shouldValidRub() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[2].currency", equalTo("RUB"))  ;
+    }
+
+    @Test
+    void shouldValidUsd() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[1].currency", equalTo("USD"))  ;
     }
 }
